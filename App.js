da0e6106c4bb9c42fbf37943cc7b65ddf2051355
FIC <brunovidal8921@gmail.com>
2020-11-20 16:22:13 -0300

    hello world

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Text>Bem Vindo ao meu App</Text>
+
+    <TextInput
+        style={styles.input}
+        placeholder="Digite seu Email"
+    />
+
+    <TextInput
+        style={styles.input}
+        placeholder="Digite sua Senha"
+    />
+
+    <TouchableOpacity style={styles.button}>
+            <Text style={{color: 'white'}}>Entrar</Text>
+    </TouchableOpacity>
+</View>
   );
 }
 
@@ -18,4 +32,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+      width: '90%',
+      height: 40,
+      justifyContent: 'center', 
+      borderColor: 'gray', 
+      borderWidth: 1,
+      borderRadius: 6,
+      padding: 10,
+      marginTop: 10
+  },
+  button: {
+      width: '70%',
+      height: 40,
+      alignItems: "center",
+      justifyContent: 'center',
+      backgroundColor: '#000000', 
+      borderColor: 'grey',
+      borderWidth: 1,
+      borderRadius: 6,
+      padding: 10,
+      marginTop: 10
+  }
 });
